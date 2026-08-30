2fc21dcd94c709e52eed4073924932364909b5d5
31kimchangki31-spec <31kimchangki31@gmail.com>
2026-08-30 19:12:29 +0900

    Update coupang_api.py

--- coupang_api.py
+++ coupang_api.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-쿠팡파트너스 Open API 연동 모듈 (골드박스 + 딥링크)
+쿠팡파트너스 Open API 연동 모듈 (골드박스 조회 + 딥링크 변환)
 """
 import os
 import time
@@ -13,9 +13,8 @@ DOMAIN = "https://api-gateway.coupang.com"
 
 
 def generate_hmac(method: str, url: str, secret_key: str, access_key: str) -> str:
-    """쿠팡 Open API 인증 헤더(Authorization) 생성"""
+    """쿠팡 Open API 인증 헤더(Authorization) 생성 (크로스플랫폼: tzset 대신 gmtime 사용)"""
     path, *query = url.split("?")
-    # time.tzset()은 유닉스 전용이라 윈도우에서 에러남 -> gmtime()으로 UTC를 직접 구해 크로스플랫폼 호환
     utc_now = time.gmtime()
     datetime_str = time.strftime("%y%m%d", utc_now) + "T" + time.strftime("%H%M%S", utc_now) + "Z"
     message = datetime_str + method + path + (query[0] if query else "")
